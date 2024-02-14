2a9f4d19544037d68525ace8095b0e4c59875b84
Victor Hugo Bernardino <vhugoz1@hotmail.com>
2024-02-14 15:42:07 -0300

    fix: env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MOVIEDB_API_KEY: process.env.MOVIEDB_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
